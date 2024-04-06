163b59ee9a313b603cd881fa3e7229a9179b0efb
Francis7575 <francis.albert.2009@gmail.com>
2024-04-05 18:07:52 -0700

    updated

--- react-pricing-toggle/vite.config.ts
+++ react-pricing-toggle/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Pricing-component-with-toggle/",
+  base: "/pricing-with-toggle/",
   plugins: [react()],
 })
